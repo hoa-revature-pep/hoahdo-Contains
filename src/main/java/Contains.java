9c3afeec41f681f656da7789e0fd9f9fa07e68bf
Hoa Do <hoa.do.ma@gmail.com>
2023-08-03 20:26:49 +0000

    Completed Challenge: Contains

--- src/main/java/Contains.java
+++ src/main/java/Contains.java
@@ -1,16 +1,30 @@
 
-
 public class Contains {
 
     /**
      * This method should search an array to see if that array contains some value.
-     * You will need to use a for loop to check every value of the array to see if it matches the target value.
+     * You will need to use a for loop to check every value of the array to see if
+     * it matches the target value.
      *
-     * @param arr array to be manipulated.
+     * @param arr    array to be manipulated.
      * @param target the value that the method should be searching for.
      * @return true if arr contains target, false otherwise.
      */
-    public boolean arrayContains(int[] arr, int target){
+    public boolean arrayContains(int[] arr, int target) {
+
+        for(int i = 0; i < arr.length; i++) {
+            boolean doesContain = (target == arr[i]);
+
+            if(doesContain) {
+                return true;
+            }
+            
+        }
+
         return false;
     }
 }
+
+// check each value of array with target
+// if target is found in array, return true
+// if target not found in array, return false
